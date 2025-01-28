754a54b4c25901ec419d12ed86ecb1132aa55e37
akisilv <akisil@vivasg.com>
2025-01-28 17:30:56 +0200

    Replace default mock with custom markup, add basic components, add Angular Material and Mterial Symbols.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+
+import { HeaderComponent } from "./core/header/header.component";
+import { SidenavMenuComponent } from "./core/sidenav-menu/sidenav-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    MatSidenavModule,
+    SidenavMenuComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular_v19_pwa_mockup';
 }
---
+++ src/app/core/sidenav-menu/sidenav-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-menu',
+  imports: [],
+  templateUrl: './sidenav-menu.component.html',
+  styleUrl: './sidenav-menu.component.scss'
+})
+export class SidenavMenuComponent {
+
+}
