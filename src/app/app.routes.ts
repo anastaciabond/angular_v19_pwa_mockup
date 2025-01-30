64ff6e6ee9f19ca4bd10e09fb303d43bc43f3b59
akisilv <akisil@vivasg.com>
2025-01-30 15:39:46 +0200

    Add pages routing in sidenav-menu.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { PricesTableComponent } from './prices/prices-table/prices-table.component';
+
+export const routes: Routes = [
+  { path: "", redirectTo: "prices/list", pathMatch: "full" },
+  { path: "user/list", component: UserListComponent, data: {title: "Users List"} },
+  { path: "user/:id", component: UserDetailComponent, data: {title: "User"} },
+  { path: "prices/list", component: PricesTableComponent, data: {title: "Prices Table"} },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
+})
+
+export class AppRoutingModule { }
--- src/app/core/sidenav-menu/sidenav-menu.component.ts
+++ src/app/core/sidenav-menu/sidenav-menu.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MatListItem, MatListItemIcon, MatListItemTitle, MatNavList } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface Section {
+  name: string;
+  name_icon: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-sidenav-menu',
-  imports: [],
+  imports: [
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    RouterLinkActive,
+    MatListItemIcon,
+  ],
   templateUrl: './sidenav-menu.component.html',
   styleUrl: './sidenav-menu.component.scss'
 })
 export class SidenavMenuComponent {
+  public pages: Section[];
 
+  constructor(
+  ) {
+    this.pages = [
+      { name: "Users list", link: "/user/list", name_icon: "view_headline" },
+      { name: "Prices", link: "prices/list", name_icon: "attach_money" },
+    ];
+  }
 }
