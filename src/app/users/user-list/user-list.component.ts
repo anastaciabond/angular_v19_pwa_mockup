c89aba9fab7729601c14ea8bdacede00e6eac8b4
akisilv <akisil@vivasg.com>
2025-02-03 15:29:00 +0200

    Add AG Grid basic table and styles for fluid layout.

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatAnchor } from '@angular/material/button';
+import { AgGridTableComponent } from '../../shared/ag-grid-table/ag-grid-table.component';
 
 @Component({
   selector: 'app-user-list',
   imports: [
     MatAnchor,
+    AgGridTableComponent,
   ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
