c092feeb120189e9741155d49c483ec60b6675a8
akisilv <akisil@vivasg.com>
2025-02-07 18:13:07 +0200

    Add external input filter, pagination, go to detail page by click.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+  ]
 };
--- src/app/shared/ag-grid-table/ag-grid-table.component.ts
+++ src/app/shared/ag-grid-table/ag-grid-table.component.ts
@@ -1,45 +1,121 @@
 import { Component } from '@angular/core';
 
 import { AgGridAngular } from 'ag-grid-angular';
-import { ColDef } from 'ag-grid-community';
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import {
+  ClientSideRowModelModule,
+  ColDef,
+  ExternalFilterModule,
+  GridApi,
+  GridReadyEvent,
+  ModuleRegistry,
+  QuickFilterModule,
+  PaginationModule,
+  RowClickedEvent,
+} from "ag-grid-community";
+import { FormsModule } from '@angular/forms';
+import { MatTooltip } from '@angular/material/tooltip';
 
-ModuleRegistry.registerModules([AllCommunityModule]);
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { Router } from '@angular/router';
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  ExternalFilterModule,
+  QuickFilterModule,
+  PaginationModule,
+]);
 
 // Row Data Interface
 interface IRow {
-  make: string;
-  model: string;
-  price: number;
-  electric: boolean;
+  login: string;
+  email: string;
+  wamp_role: string;
+  roles: string;
+  portfolios: string;
+  group: string;
 }
 
 @Component({
   selector: 'app-ag-grid-table',
-  imports: [AgGridAngular],
+  imports: [
+    AgGridAngular,
+    FormsModule,
+    MatTooltip,
+    MatButtonModule,
+    MatInputModule,
+  ],
   templateUrl: './ag-grid-table.component.html',
   styleUrl: './ag-grid-table.component.scss'
 })
 export class AgGridTableComponent {
+  public externalFilterValue: string = "";
+
+  private gridApi!: GridApi<IRow>;
+
   // Row Data: The data to be displayed.
   rowData: IRow[] = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-    { make: "Fiat", model: "500", price: 15774, electric: false },
-    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+    { login: "apak@summitalpha.com", email: "apak@summitalpha.com", wamp_role: "client", roles: "System Presets Role", portfolios: "AX Technology-0120", group: "group" },
+    { login: "Tesla", email: "email Y", wamp_role: "value", roles: "value", portfolios: "value", group: "wwwvalue" },
+    { login: "Ford", email: "F-Series", wamp_role: "value", roles: "value", portfolios: "value", group: "xxxvalue" },
+    { login: "Toyota", email: "Corolla", wamp_role: "value", roles: "value", portfolios: "value", group: "zzzvalue" },
+    { login: "Mercedes", email: "EQA", wamp_role: "value", roles: "value", portfolios: "value", group: "qqqvalue" },
+    { login: "Fiat", email: "500", wamp_role: "value", roles: "value", portfolios: "value", group: "eeevalue" },
+    { login: "Nissan", email: "Juke", wamp_role: "value", roles: "value", portfolios: "value", group: "aaavalue" },
+    { login: "apak@summitalpha.com", email: "apak@summitalpha.com", wamp_role: "client", roles: "System Presets Role", portfolios: "AX Technology-0120", group: "group" },
+    { login: "Tesla", email: "email Y", wamp_role: "value", roles: "value", portfolios: "value", group: "wwwvalue" },
+    { login: "Ford", email: "F-Series", wamp_role: "value", roles: "value", portfolios: "value", group: "xxxvalue" },
+    { login: "Toyota", email: "Corolla", wamp_role: "value", roles: "value", portfolios: "value", group: "zzzvalue" },
+    { login: "Mercedes", email: "EQA", wamp_role: "value", roles: "value", portfolios: "value", group: "qqqvalue" },
+    { login: "Fiat", email: "500", wamp_role: "value", roles: "value", portfolios: "value", group: "eeevalue" },
+    { login: "Nissan", email: "Juke", wamp_role: "value", roles: "value", portfolios: "value", group: "aaavalue" },
+    { login: "apak@summitalpha.com", email: "apak@summitalpha.com", wamp_role: "client", roles: "System Presets Role", portfolios: "AX Technology-0120", group: "group" },
+    { login: "Tesla", email: "email Y", wamp_role: "value", roles: "value", portfolios: "value", group: "wwwvalue" },
+    { login: "Ford", email: "F-Series", wamp_role: "value", roles: "value", portfolios: "value", group: "xxxvalue" },
+    { login: "Toyota", email: "Corolla", wamp_role: "value", roles: "value", portfolios: "value", group: "zzzvalue" },
+    { login: "Mercedes", email: "EQA", wamp_role: "value", roles: "value", portfolios: "value", group: "qqqvalue" },
+    { login: "Fiat", email: "500", wamp_role: "value", roles: "value", portfolios: "value", group: "eeevalue" },
+    { login: "Nissan", email: "Juke", wamp_role: "value", roles: "value", portfolios: "value", group: "aaavalue" },
   ];
 
   // Column Definitions: Defines & controls grid columns.
   colDefs: ColDef<IRow>[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" },
+    {
+      field: "login",
+    },
+    { field: "email" },
+    { field: "wamp_role" },
+    { field: "roles" },
+    { field: "portfolios" },
+    { field: "group" },
   ];
 
   defaultColDef: ColDef = {
     flex: 1,
   };
+
+  constructor(private router: Router) {
+  }
+
+  public onFilterTextBoxChanged(value: string) {
+    //this.gridApi.setGridOption("quickFilterText", (document.getElementById("filter-text-box") as HTMLInputElement).value);
+    this.gridApi.setGridOption("quickFilterText", value);
+  }
+
+  public clearFilter(value: string): void {
+    this.onFilterTextBoxChanged(value);
+    this.externalFilterValue = "";
+  }
+
+  public onGridReady(params: GridReadyEvent<IRow>) {
+    this.gridApi = params.api;
+  }
+
+  public onRowClicked(event: RowClickedEvent) {
+    console.log("Row was clicked");
+    this.router.navigate(["/user/:id"])
+      .catch((error: Error) => {
+        console.error(error);
+      });
+  }
 }
